ffaebb7b504942995dbb46f57adf1f1490c28075
Anthony <anth06ny@gmail.com>
2023-06-16 14:28:14 +0100

    Correction TP RecyclerView

--- app/src/main/java/com/amonteiro/myapplication/WindListAdapter.kt
+++ app/src/main/java/com/amonteiro/myapplication/WindListAdapter.kt
@@ -7,28 +7,25 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.amonteiro.myapplication.databinding.RowCoordBinding
 
-class CoordListAdapter : ListAdapter<CoordBean, CoordListAdapter.ViewHolder>(Comparator()) {
+class WindListAdapter : ListAdapter<WindBean, WindListAdapter.ViewHolder>(Comparator()) {
 
     class ViewHolder(val binding:RowCoordBinding) : RecyclerView.ViewHolder(binding.root)
 
-    class Comparator : DiffUtil.ItemCallback<CoordBean>() {
-        override fun areItemsTheSame(oldItem: CoordBean, newItem: CoordBean)
+    class Comparator : DiffUtil.ItemCallback<WindBean>() {
+        override fun areItemsTheSame(oldItem: WindBean, newItem: WindBean)
         = oldItem === newItem
 
-        override fun areContentsTheSame(oldItem: CoordBean, newItem: CoordBean)
+        override fun areContentsTheSame(oldItem: WindBean, newItem: WindBean)
         = oldItem == newItem
 
     }
 
-    //Création d'une ligne (20fois)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-    //instanciation de la ligne
         =ViewHolder(RowCoordBinding.inflate(LayoutInflater.from(parent.context)))
 
 
-    //Remplissage d'une ligne (infini)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
-        holder.binding.tvville.text = "${item.lat}, ${item.lon}"
+        holder.binding.tvville.text = "${item.speed}"
     }
 }
